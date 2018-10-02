e926a8423a9f7e2240d75d2f90c68c0548a05193
Borshchnabe <borshchnabe@gmail.com>
2018-10-03 00:27:01 +0900

    Borshch
    predictionServiceの実装

--- ソースコード/test/tensorflow00/predictionService.py
+++ ソースコード/test/tensorflow00/predictionService.py
@@ -8,43 +8,47 @@ import const
 import sys
 from makeStudyData import GetDate
 import numpy as np
-import datetime
+from datetime import datetime
 from mongodb_write import insertCollection
-
-
-# 定数型の文字列を呼び出す(OPEN,CLOSE,HIGH,LOW)が入ってる　例：c.OPEN
-c = sys.modules["const"]
-
-# 各値の予測値を取得
-OPEN = makePredictionModel(c.OPEN)
-CLOSE = makePredictionModel(c.CLOSE)
-HIGH = makePredictionModel(c.HIGH)
-LOW = makePredictionModel(c.LOW)
-
-#　時間を取得→実行時間の設定に使うかも？
-TIME = GetDate()
-
-# 予測値だけを取り出す処理
-OPEN = OPEN[:,0].astype(np.float64)
-CLOSE = CLOSE[:,0].astype(np.float64)
-HIGH = HIGH[:,0].astype(np.float64)
-LOW = LOW[:,0].astype(np.float64)
-
-# 辞書キーの作成
-d = {"time":TIME, "close":CLOSE[0], "open":OPEN[0], "high":HIGH[0], "low":LOW[0]}
-
-result1 = insertCollection("P_USD_JPY_RATE",d)
-
-
 
 # 下の層はオアンダの履歴を遡ってデータを取得し、データを加工、USD_JPY_RATEというテーブルの中へ格納する
 import oandapy
 import pandas as pd
-import datetime as dt
 
 oanda = oandapy.API(environment="practice", access_token="806baeb6718f153657980002fea49c6c-2cf6534cb404c014c63931f73fa3def7")
 
 response1 = oanda.get_history(instrument="USD_JPY", granularity="D", count=1)
 USD_JPY_D1 = response1.get("candles")
+
+time = USD_JPY_D1[0].get("time")[0:10].replace('-', '/')
+weekday = datetime.strptime(time.replace('/', '-') + " 06:00:00", '%Y-%m-%d %H:%M:%S').weekday()
+
+# 登録するための辞書作成
+d = {"time":time, "close":USD_JPY_D1[0].get('closeBid'), 'open':USD_JPY_D1[0].get('openBid'), 'high':USD_JPY_D1[0].get('highBid'), 'low':USD_JPY_D1[0].get('lowBid'),'volume':USD_JPY_D1[0].get('volume'),'weekday':weekday}
+
+result1 = insertCollection("USD_JPY_RATE",d)
+
+# 定数型の文字列を呼び出す(OPEN,CLOSE,HIGH,LOW)が入ってる　例：c.OPEN
+c = sys.modules["const"]
+
+# 各値の予測値を取得
+P_OPEN = makePredictionModel(c.OPEN)
+P_CLOSE = makePredictionModel(c.CLOSE)
+P_HIGH = makePredictionModel(c.HIGH)
+P_LOW = makePredictionModel(c.LOW)
+
+#　時間を取得→実行時間の設定に使うかも？
+P_TIME = GetDate()
+
+# 予測値だけを取り出す処理
+P_OPEN = P_OPEN[:,0].astype(np.float64)
+P_CLOSE = P_CLOSE[:,0].astype(np.float64)
+P_HIGH = P_HIGH[:,0].astype(np.float64)
+P_LOW = P_LOW[:,0].astype(np.float64)
+
+# 辞書キーの作成
+p= {"time":P_TIME, "close":P_CLOSE[0], "open":P_OPEN[0], "high":P_HIGH[0], "low":P_LOW[0]}
+
+result1 = insertCollection("P_USD_JPY_RATE",p)
 
 
